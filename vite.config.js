5d2d0238b9cbfc0f00b2faf2f2ab055023c4bdb6
Praveen Pandikaran <praveenpandikaran@Praveens-MacBook-Air.local>
2025-11-05 20:43:30 -0600

    updated build

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
 plugins: [react()],
-base: '/CS571-WebDevProject/',
+base: '/p173/',
 build: {
 outDir: 'docs'
 }
