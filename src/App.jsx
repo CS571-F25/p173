506398983c357f558c8ba1c07193cd55d68bbbf7
Praveen Pandikaran <praveenpandikaran@Praveens-MacBook-Air.local>
2025-11-05 19:57:19 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { HashRouter, Routes, Route } from 'react-router'
+import Home from './components/Home'
+import AboutMe from './components/AboutMe'
+
+function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<AboutMe/>} />
+      </Routes>
+    </HashRouter>
+  )
+}
+
+export default App
